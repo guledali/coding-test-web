e36ed294f47a689882fee9f5ca8b037f687574e9
guledali <guled.ali01@gmail.com>
2025-01-14 17:32:19 +0100

    Restore the api/companies/index file as it was

--- pages/api/companies/index.ts
+++ pages/api/companies/index.ts
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { COMPANIES } from "@/app/lib/mock";
 
-export default function handler(res: NextApiResponse<any>) {
-  res.status(200).json({ ...COMPANIES });
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  res.status(200).json({ data: COMPANIES });
 }
