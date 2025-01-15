4a102763dd407330b60dc271cf64ec94ee594d17
guledali <guled.ali01@gmail.com>
2025-01-15 13:28:30 +0100

    Adding Github action workflow for the project

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
     // Base URL to use in actions like `await page.goto('/')`
     baseURL: baseUrl,
     trace: "on-first-retry",
-    headless: false,
+    headless: process.env.CI ? true : false,
   },
 
   projects: [
